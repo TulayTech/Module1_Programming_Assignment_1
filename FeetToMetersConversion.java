0f23a5f2af537f2c95fe6559a506dbd1a7c7bb66
TuTech <tulay@tutechit.com>
2025-11-03 08:31:04 -0500

    Started Programming Exercise 6.9
    - Added comments to explain the program
    - Created public class FeetToMetersConversion

---
+++ FeetToMetersConversion.java
@@ -0,0 +1,17 @@
+package Module1_Programming_Assignment_1;
+/**
+Chapter 6:  Programming Exercise 6.9
+
+This program defines two conversion methods:
+    - footToMeter
+    - meterToFoot
+
+Then prints a side by side table to display the conversion results.
+The formula for the conversion is:
+    - meter = 0.305 * foot
+    - foot = 3.279 * meter
+*/
+
+public class FeetToMetersConversion {
+    
+}
